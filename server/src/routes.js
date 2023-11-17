2aa56bd66a8374cb9ac093c32e20388b9a2b8e08
LukyMDI <97931181+LukyMDI@users.noreply.github.com>
2023-11-17 19:21:02 -0300

    config routes

--- server/src/routes.js
+++ server/src/routes.js
@@ -4,10 +4,6 @@ const routes = express.Router();
 // Inserção dos Controllers
 const productController = require("./controllers/productController");
 
-routes.get("/", (req, res) => {
-    res.send("<h1>Está funcionando!</h1>");
-});
-
 routes.post("/api/products/create", productController.create);
 routes.get("/api/products/view", productController.read);
 routes.post("/api/products/delete", productController.delete);
